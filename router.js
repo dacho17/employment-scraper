0e8bf5a5726bdd4700735c0023f114ec7d9be1ee
dacho <dosenovicdavid7@gmail.com>
2023-03-21 03:33:35 +0100

    Route openned for CareerBuilder Scraping

--- router.js
+++ router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const lnAdScraper = require('./scrapers/LNadScraper');
 const lnDetailedScraper = require('./scrapers/LNdetailedScraper');
 const indeedScraper = require('./scrapers/indeedScraper');
+const careerBuilderAdScraper = require('./scrapers/careerBuilderAdScraper');
 
 router.post('/scrape-linkedin-ads', (req, res) => {
     lnAdScraper.doAscrape(req.body.jobTitle, req.body.location, req.body.nOfAds).then(responseObj => {
@@ -19,11 +20,16 @@ router.get('/scrape-linkedin-details', (req, res) => {
 
 router.post('/scrape-indeed', (req, res) => {
     console.log(req.body.jobTitle);
-    indeedScraper.doAscrape(req.body.jobTitle).then(result => {
-        res.status(200).json(result)
-    }, err => {
-        res.status(500).json(err)
-    })
+    indeedScraper.doAscrape(req.body.jobTitle).then(responseObj => {
+        res.status(responseObj.statusCode).json(responseObj.message);
+    });
+});
+
+router.post('/scrape-career-builder', (req, res) => {
+    careerBuilderAdScraper.doAscrape(req.body.workFromHome, req.body.jobTitle, req.body.nOfAds).then(responseObj => {
+        // res.status(responseObj.statusCode).json(responseObj.message);
+        res.status(200).json('zz');
+    });
 });
 
 module.exports = router;
