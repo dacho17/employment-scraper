1fcfae8ae157a170b746c312514ec8483888f830
dacho <dosenovicdavid7@gmail.com>
2023-03-24 09:56:40 +0100

    Changes to the BrowserAPI Class

--- src/browserAPI.ts
+++ src/browserAPI.ts
@@ -1,34 +1,33 @@
 import puppeteer from 'puppeteer';
 
 export default class Browser {
-    browser: any | null;
-    page: any | null;
+    // browser: any | null;
+    // page: any | null;
 
-    async run(): Promise<any> {
-        this.browser = await puppeteer.launch();
-        this.page = null;   // no page openned at the beginning
+    static async run(): Promise<any> {
+        // this.browser = ;
+        // this.page = null;   // no page openned at the beginning
         
-        return this.browser;
+        return await puppeteer.launch();
     }
 
-    async close(): Promise<void> {
-        await this.browser.close();
-        this.browser = null;
+    static async close(browser): Promise<void> {
+        await browser.close();
     }
 
-    async openPage(url: string): Promise<void> {
-        this.page = await this.browser.newPage();
-        await this.page.goto(url);
-        await this.page.setViewport({width: 1080, height: 1024});
+    static async openPage(browser, url: string): Promise<any> {
+        const page = await browser.newPage();
+        await page.goto(url);
+        await page.setViewport({width: 1080, height: 1024});
+        return page;
     }
 
-    async getDataFrom(selector: string): Promise<string | null> {
-        await this.page.waitForSelector(selector);
-        const dataElement = await this.page.$(selector);
+    static async getDataFrom(page, selector: string): Promise<string | null> {
+        await page.waitForSelector(selector);
+        const dataElement = await page.$(selector);
         const data = await dataElement.evaluate(el => el.textContent);
         await dataElement.dispose();
     
         return data;
     }
-
 }
